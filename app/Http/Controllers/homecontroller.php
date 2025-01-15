c52f74a549e45d9a356ea4abb92ca111381cdada
rahim8050 <rahimranxx8050@gmail.com>
2025-01-15 19:33:28 +0000

    commits

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class homecontroller extends Controller
 {
+    User::factory()->count(10)->sequence(
+            ['name'=>'Mercy'],
+            ['name'=>'John'],
+         )->create();
+    
     public function index(){
         return view('car.index');
     }
---
+++ database/seeders/MakerSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MakerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
