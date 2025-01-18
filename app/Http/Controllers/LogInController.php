f8e6fcd54e4e1a018311e55e7aa96541e70d2424
rahim8050 <rahimranxx8050@gmail.com>
2025-01-18 20:49:37 +0000

    solids

--- app/Http/Controllers/LogInController.php
+++ app/Http/Controllers/LogInController.php
@@ -6,9 +6,8 @@ use Illuminate\Http\Request;
 
 class LogInController extends Controller
 {
-    public function create(){
-        return view("login");
-    }
-    public function details{
-    'email'=>'email'
+
+public function details() {
+    return ['email' => 'email'];
 }
+?>
