24ca80219d796a447dae1960e4ffb02f60ebeaee
rahim8050 <rahimranxx8050@gmail.com>
2025-01-31 14:23:03 +0000

    db

---
+++ database/migrations/2025_01_31_141835_create_doubles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doubles', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('company');
+            $table->string('model');
+            $table->string('color');
+            $table->string('plate');
+            $table->string('year');
+            $table->string('engine');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doubles');
+    }
+};
