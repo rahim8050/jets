b67fdf88951f5042322e310f07f77c584043163e
rahim8050 <rahimranxx8050@gmail.com>
2025-02-26 18:44:46 +0000

    ins

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+@if(session()->has('message'))
+<di x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+    class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
+    <p>
+      {{session('message')}}
+    </p>
+  </div>
+    
+@endif
